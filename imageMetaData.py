e7329e00d782be0dd20ee417eb2d1f48d9bd0971
VenusMagar <67140773+VenusMagar@users.noreply.github.com>
2021-11-03 15:55:34 +0545

    Add files via upload

---
+++ imageMetaData.py
@@ -0,0 +1,26 @@
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+def getImageMetaData(imgName):
+    try:
+        metaData = {}
+        imgFile = Image.open(imgName)
+        print("Obtaining Metadata From the Image")
+        imageMetaData = imgFile._getexif()
+        
+        if imageMetaData:
+            print("Metadata Obtained")
+
+            for(tag, value) in imageMetaData.items():
+                tagname = TAGS.get(tag, tag)
+                metaData[tagname] = value
+                print(tagname, value)
+        
+    except:
+        print("Failed To Obtain The Metadata")
+
+def executeProgram():
+    imageFilePath = input("Enter the file path of the image : ")
+    getImageMetaData(imageFilePath)
+
+executeProgram()
